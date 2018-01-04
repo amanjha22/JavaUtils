06564ee6eda112ddf2bd43c0e042df2640a7f83c
Aman Jha <amanjha22@gmail.com>
2018-01-04 19:58:58 +0530

    Add methods to accept request body

--- src/main/java/Http/HttpCaller.java
+++ src/main/java/Http/HttpCaller.java
@@ -11,107 +11,322 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class HttpCaller {
-    public static Map<String, String> doHttpCall(String url, HttpRequestMethod httpRequestMethod) throws IOException {
-        return doHttpCall(url, null, httpRequestMethod, null, 0, TimeUnit.MILLISECONDS);
-    }
+    	/**
+	 * @param url {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, HttpRequestMethod httpRequestMethod)
+			throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, null, httpRequestMethod, null, null, 0, TimeUnit.MILLISECONDS);
+	}
 
-    public static Map<String, String> doHttpCall(String url, HttpRequestMethod httpRequestMethod, long timeout, TimeUnit timeUnit) throws IOException {
-        return doHttpCall(url, null, httpRequestMethod, null, timeout, timeUnit);
-    }
+	/**
+	 * @param url {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param timeout {@link Long}
+	 * @param timeUnit {@link TimeUnit}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, HttpRequestMethod httpRequestMethod, long timeout,
+			TimeUnit timeUnit) throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, null, httpRequestMethod, null, null, timeout, timeUnit);
+	}
 
-    public static Map<String, String> doHttpCall(String url, Map<String, String> params, HttpRequestMethod httpRequestMethod) throws IOException {
-        return doHttpCall(url, params, httpRequestMethod, null, 0, TimeUnit.MILLISECONDS);
-    }
+	/**
+	 * @param url {@link String}
+	 * @param params {@link Map} of {@link String} vs {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, Map<String, String> params,
+			HttpRequestMethod httpRequestMethod) throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, params, httpRequestMethod, null, null, 0, TimeUnit.MILLISECONDS);
+	}
 
-    public static Map<String, String> doHttpCall(String url, Map<String, String> params, HttpRequestMethod httpRequestMethod, long timeout, TimeUnit timeUnit) throws IOException {
-        return doHttpCall(url, params, httpRequestMethod, null, timeout, timeUnit);
-    }
+	/**
+	 * @param url {@link String}
+	 * @param params {@link Map} of {@link String} vs {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param timeout {@link Long}
+	 * @param timeUnit {@link TimeUnit}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, Map<String, String> params,
+			HttpRequestMethod httpRequestMethod, long timeout, TimeUnit timeUnit)
+			throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, params, httpRequestMethod, null, null, timeout, timeUnit);
+	}
 
-    public static Map<String, String> doHttpCall(String url, HttpRequestMethod httpRequestMethod, Map<String, String> headers) throws IOException {
-        return doHttpCall(url, null, httpRequestMethod, headers, 0, TimeUnit.MILLISECONDS);
-    }
+	/**
+	 * @param url {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param headers {@link Map} of {@link String} vs {@link String}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, HttpRequestMethod httpRequestMethod,
+			Map<String, String> headers) throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, null, httpRequestMethod, null, headers, 0, TimeUnit.MILLISECONDS);
+	}
 
-    public static Map<String, String> doHttpCall(String url, HttpRequestMethod httpRequestMethod, Map<String, String> headers, long timeout, TimeUnit timeUnit) throws IOException {
-        return doHttpCall(url, null, httpRequestMethod, headers, timeout, timeUnit);
-    }
+	/**
+	 * @param url {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param headers {@link Map} of {@link String} vs {@link String}
+	 * @param timeout {@link Long}
+	 * @param timeUnit {@link TimeUnit}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, HttpRequestMethod httpRequestMethod,
+			Map<String, String> headers, long timeout, TimeUnit timeUnit)
+			throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, null, httpRequestMethod, null, headers, timeout, timeUnit);
+	}
 
-    public static Map<String, String> doHttpCall(String url, Map<String, String> params, HttpRequestMethod httpRequestMethod, Map<String, String> headers) throws IOException {
-        return doHttpCall(url, params, httpRequestMethod, headers, 0, TimeUnit.MILLISECONDS);
-    }
+	/**
+	 * @param url {@link String}
+	 * @param params {@link Map} of {@link String} vs {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param headers {@link Map} of {@link String} vs {@link String}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, Map<String, String> params,
+			HttpRequestMethod httpRequestMethod, Map<String, String> headers)
+			throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, params, httpRequestMethod, null, headers, 0, TimeUnit.MILLISECONDS);
+	}
 
-    public static Map<String, String> doHttpCall(String url, Map<String, String> params, HttpRequestMethod httpRequestMethod, Map<String, String> headers, long timeout, TimeUnit timeUnit) throws IOException {
-        Long timeOutInMillis = TimeUnit.MILLISECONDS.convert(timeout, timeUnit);
+	/**
+	 * @param url {@link String}
+	 * @param requestBody String
+	 * @param httpRequestMethod the HTTP request method
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, String requestBody, HttpRequestMethod httpRequestMethod)
+			throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, null, httpRequestMethod, requestBody, null, 0, TimeUnit.MILLISECONDS);
+	}
 
-        if (HttpRequestMethod.GET.equals(httpRequestMethod) && params != null) {
-            String paramString = ParameterStringBuilder.getParamsString(params);
-            url = url + "?" + paramString;
-        }
+	/**
+	 * @param url {@link String}
+	 * @param requestBody String
+	 * @param httpRequestMethod the HTTP request method
+	 * @param timeout {@link Long}
+	 * @param timeUnit {@link TimeUnit}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, String requestBody, HttpRequestMethod httpRequestMethod,
+			long timeout, TimeUnit timeUnit) throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, null, httpRequestMethod, requestBody, null, timeout, timeUnit);
+	}
 
-        URL httpURL = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) httpURL.openConnection();
-        connection.setRequestMethod(httpRequestMethod.getValue());
+	/**
+	 * @param url {@link String}
+	 * @param requestBody String
+	 * @param params {@link Map} of {@link String} vs {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, Map<String, String> params, String requestBody,
+			HttpRequestMethod httpRequestMethod) throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, params, httpRequestMethod, requestBody, null, 0, TimeUnit.MILLISECONDS);
+	}
 
-        connection.setRequestProperty("User-Agent", "JAVA_HTTP_REQUEST");
+	/**
+	 * @param url {@link String}
+	 * @param requestBody String
+	 * @param params {@link Map} of {@link String} vs {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param timeout {@link Long}
+	 * @param timeUnit {@link TimeUnit}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, Map<String, String> params, String requestBody,
+			HttpRequestMethod httpRequestMethod, long timeout, TimeUnit timeUnit)
+			throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, params, httpRequestMethod, requestBody, null, timeout, timeUnit);
+	}
 
-        //set url params in case of non-get requests
-        if (!HttpRequestMethod.GET.equals(httpRequestMethod) && params != null && params.size() != 0) {
-            connection.setDoOutput(true); //required if params needs to be set
-            DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
-            dataOutputStream.writeBytes(ParameterStringBuilder.getParamsString(params));
-            dataOutputStream.flush();
-            dataOutputStream.close();
-        }
+	/**
+	 * @param url {@link String}
+	 * @param requestBody  {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param headers {@link Map} of {@link String} vs {@link String}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, String requestBody, HttpRequestMethod httpRequestMethod,
+			Map<String, String> headers) throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, null, httpRequestMethod, requestBody, headers, 0, TimeUnit.MILLISECONDS);
+	}
 
-        //set request headers
-        if (headers != null) {
-            for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
-                connection.setRequestProperty(headerEntry.getKey(), headerEntry.getValue());
-            }
-        }
+	/**
+	 * @param url {@link String}
+	 * @param requestBody {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param headers {@link Map} of {@link String} vs {@link String}
+	 * @param timeout {@link Long}
+	 * @param timeUnit {@link TimeUnit}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, String requestBody, HttpRequestMethod httpRequestMethod,
+			Map<String, String> headers, long timeout, TimeUnit timeUnit)
+			throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, null, httpRequestMethod, requestBody, headers, timeout, timeUnit);
+	}
 
-        //set connection timeout
-        if (timeOutInMillis != 0)
-            connection.setConnectTimeout(timeOutInMillis.intValue());
+	/**
+	 * @param url {@link String}
+	 * @param params {@link Map} of {@link String} vs {@link String}
+	 * @param requestBody {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param headers {@link Map} of {@link String} vs {@link String}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException  Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, Map<String, String> params, String requestBody,
+			HttpRequestMethod httpRequestMethod, Map<String, String> headers)
+			throws IOException, HttpRequestMethodNotSupportedException
+	{
+		return doHttpCall(url, params, httpRequestMethod, requestBody, headers, 0, TimeUnit.MILLISECONDS);
+	}
 
-        int statusCode = connection.getResponseCode();
-        String responseMessage= connection.getResponseMessage();
+	/**
+	 * @param url {@link String}
+	 * @param params {@link Map} of {@link String} vs {@link String}
+	 * @param httpRequestMethod the HTTP request method
+	 * @param requestBody {@link String}
+	 * @param headers {@link Map} of {@link String} vs {@link String}
+	 * @param timeout {@link Long}
+	 * @param timeUnit {@link TimeUnit}
+	 * @return {@link Map} of {@link String} vs {@link String}
+	 * @throws IOException Throws {@link IOException}
+	 * @throws HttpRequestMethodNotSupportedException Throws {@link HttpRequestMethodNotSupportedException}
+	 */
+	public static Map<String, String> doHttpCall(String url, Map<String, String> params,
+			HttpRequestMethod httpRequestMethod, String requestBody, Map<String, String> headers, long timeout,
+			TimeUnit timeUnit) throws IOException, HttpRequestMethodNotSupportedException
+	{
+		Long timeOutInMillis = TimeUnit.MILLISECONDS.convert(timeout, timeUnit);
+		if (HttpRequestMethod.GET.equals(httpRequestMethod) && !Strings.isNullOrEmpty(requestBody))
+			throw new HttpRequestMethodNotSupportedException("Request Body not supported for Get Requests");
 
-        //Read the response only if response code is 200
-        StringBuffer responseString = new StringBuffer();
-        if(statusCode==200 || statusCode==201) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                responseString.append(inputLine);
-            }
-            in.close();
-        }
+		if (params != null)
+		{
+			String paramString = ParameterStringBuilder.getParamsString(params);
+			url = url + "?" + paramString;
+		}
 
-        //Disconnect the connection
-        connection.disconnect();
+		URL httpURL = new URL(url);
+		HttpURLConnection connection = (HttpURLConnection) httpURL.openConnection();
+		connection.setRequestMethod(httpRequestMethod.getValue());
 
-        Map<String, String> response= new HashMap<>();
-        response.put("response", responseString.toString());
-        response.put("response-code", String.valueOf(statusCode));
-        response.put("response-message",responseMessage);
+		connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+		connection.setRequestProperty("User-Agent", "JAVA_HTTP_REQUEST");
 
-        return response;
-    }
+		//set request headers
+		if (headers != null)
+		{
+			for (Map.Entry<String, String> headerEntry : headers.entrySet())
+			{
+				connection.setRequestProperty(headerEntry.getKey(), headerEntry.getValue());
+			}
+		}
 
-    enum HttpRequestMethod {
-        POST("POST"),
-        GET("GET"),
-        PUT("PUT"),
-        DELETE("DELETE");
+		//set url params in case of non-get requests
+		if (!HttpRequestMethod.GET.equals(httpRequestMethod) && !Strings.isNullOrEmpty(requestBody))
+		{
+			connection.setDoOutput(true); //required if params needs to be set
+			DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+			dataOutputStream.writeBytes(requestBody);
+			dataOutputStream.flush();
+			dataOutputStream.close();
+		}
 
-        String val;
+		//set connection timeout
+		if (timeOutInMillis != 0)
+			connection.setConnectTimeout(timeOutInMillis.intValue());
 
-        HttpRequestMethod(String val) {
-            this.val = val;
-        }
+		int statusCode = connection.getResponseCode();
+		String responseMessage = connection.getResponseMessage();
 
-        public String getValue() {
-            return this.val;
-        }
-    }
+		//Read the response only if response code is 200 or 201
+		StringBuffer responseString = new StringBuffer();
+		if (statusCode == 200 || statusCode == 201)
+		{
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+			{
+				responseString.append(inputLine);
+			}
+			in.close();
+		}
+
+		//Disconnect the connection
+		connection.disconnect();
+
+		Map<String, String> response = new HashMap<>();
+		response.put("response", responseString.toString());
+		response.put("response-code", String.valueOf(statusCode));
+		response.put("response-message", responseMessage);
+
+		return response;
+	}
+
+	enum HttpRequestMethod
+	{
+		POST("POST"), GET("GET"), PUT("PUT"), DELETE("DELETE");
+
+		String val;
+
+		HttpRequestMethod(String val)
+		{
+			this.val = val;
+		}
+
+		public String getValue()
+		{
+			return this.val;
+		}
+	}
 }
